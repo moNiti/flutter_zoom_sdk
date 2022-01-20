ec75c6a89bb11d2b230484dd3c27f20f8164dc08
Niti Jira <mo@sennalabs.com>
2022-01-20 17:27:52 +0700

    integrate ios plugin to objc

--- ios/Classes/FlutterZoomSdkPlugin.m
+++ ios/Classes/FlutterZoomSdkPlugin.m
@@ -1,15 +1,28 @@
 #import "FlutterZoomSdkPlugin.h"
-#if __has_include(<flutter_zoom_sdk/flutter_zoom_sdk-Swift.h>)
-#import <flutter_zoom_sdk/flutter_zoom_sdk-Swift.h>
-#else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_zoom_sdk-Swift.h"
-#endif
+
 
 @implementation FlutterZoomSdkPlugin
+// + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+//   [SwiftFlutterZoomSdkPlugin registerWithRegistrar:registrar];
+// }
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterZoomSdkPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_zoom_sdk"
+            binaryMessenger:[registrar messenger]];
+  FlutterZoomSdkPlugin* instance = [[FlutterZoomSdkPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
